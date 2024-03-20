c7e0473663327cc1284d0c8c86ce6c03abd9b0bc
Pedro de Oliveira Machado <pedromachado.2003@alunos.utfpr.edu.br>
2024-03-19 23:14:30 -0300

    Projeto 1 criado

---
+++ Projeto 1/script.js
@@ -0,0 +1,16 @@
+
+    const button1 = document.getElementById('confirm');
+    button1.addEventListener("click", test);
+
+    function test(){
+        fetch("https://thronesapi.com/api/v2/Characters/0").then((resp) => resp.json())
+        .then((data) => showCharacter(data));
+    }
+
+    function showCharacter(character){
+        let characterList = document.getElementById("characterList");
+        let characterName = document.createElement("LI");
+        characterName.innerHTML = `Personagem: ${character.firstName}`;
+        console.log(characterName);
+        characterList.appendChild(characterName);
+    }
